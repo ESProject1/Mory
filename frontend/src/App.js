bce87a228e88af18d03ccd4aba2ff97b6bf5f590
parkeunhyo <eun261126@naver.com>
2025-01-20 15:54:42 +0900

    폰트 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
+import './App.css'; // CSS 스타일 가져오기
 
-function App() {
-
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
-      });
-  }, []) 
-
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-          {message}
-      </header>
+    <div>
+      <p style={{ fontWeight: '100' }}>이 텍스트는 100 굵기로 표시됩니다.</p>
+      <p style={{ fontWeight: '300' }}>이 텍스트는 300 굵기로 표시됩니다.</p>
+      <p style={{ fontWeight: '400' }}>이 텍스트는 400 굵기로 표시됩니다.</p>
+      <p style={{ fontWeight: '700' }}>이 텍스트는 700 굵기로 표시됩니다.</p>
+      <p style={{ fontWeight: '900' }}>이 텍스트는 900 굵기로 표시됩니다.</p>
     </div>
   );
-}
+};
 
 export default App;
