9b68c2f9d264f016df8e049c482cd435306e5efd
parkeunhyo <eun261126@naver.com>
2025-02-12 17:52:48 +0900

    완성(스프링 수정전)

--- frontend/src/components/Diary.js
+++ frontend/src/components/Diary.js
@@ -7,40 +7,41 @@ import Category from "../components/Category";
 
 const Diary = () => {
   return (
-    <body>
-      <div>
-        <header className={styles.header}>
-          <img src={MoryLogo} alt="Mory Logo" className={styles.logo} />
+    <div>
+      <header className={styles.header}>
+        <img src={MoryLogo} alt="Mory Logo" className={styles.logo} />
 
-          {/* 버튼 컨테이너 */}
-          <div className={styles.buttonContainer}>
-            <button className={styles.mypagebtn}>마이페이지</button>
-            <h3 className={styles.seperate}>|</h3>
-            <button className={styles.logoutbtn}>로그아웃</button>
-          </div>
-        </header>
+        {/* 버튼 컨테이너 */}
+        <div className={styles.buttonContainer}>
+          <button className={styles.mypagebtn}>마이페이지</button>
+          <h3 className={styles.seperate}>|</h3>
+          <button className={styles.logoutbtn}>로그아웃</button>
+        </div>
+      </header>
 
-        <main className={styles.main}>
-          <div className={styles.backgroundbox}>
-            <div className={styles.dottedbox}>
-              <div className={styles.box}></div>
-            </div>
-          </div>
+      <main className={styles.main}>
+        <div className={styles.contentContainer}>
           <div className={styles.springContainer}>
             <img src={SpringIcon} alt="Spring" className={styles.springIcon} />
             <img src={SpringIcon} alt="Spring" className={styles.springIcon} />
+          </div>
 
+          <div className={styles.backgroundbox}>
+            <div className={styles.dottedbox}>
+              <div className={styles.box}></div>
+            </div>
           </div>
           <div className={styles.backgroundbox}>
             <div className={styles.dottedbox}>
               <div className={styles.box}></div>
             </div>
           </div>
-          <Category />
+        </div>
 
-        </main>
-      </div>
-    </body>
+        <Category />
+
+      </main>
+    </div>
   );
 };
 
