21a9cde32557c1e911a9a0c6e0c45a9cfe9abdb5
parkeunhyo <eun261126@naver.com>
2025-02-12 17:57:10 +0900

    .

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './App.js';
-import Diary from './components/Diary.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Diary />
   </React.StrictMode>
 );
 
