2903ceb82173c493bfe27e9a860187d772038ed2
parkeunhyo <eun261126@naver.com>
2025-02-12 15:42:29 +0900

    화면 크기 조정

--- frontend/src/components/Diary.js
+++ frontend/src/components/Diary.js
@@ -7,7 +7,6 @@ import Category from "../components/Category";
 
 const Diary = () => {
   return (
-    <body>
       <div>
         <header className={styles.header}>
           <img src={MoryLogo} alt="Mory Logo" className={styles.logo} />
@@ -21,26 +20,26 @@ const Diary = () => {
         </header>
 
         <main className={styles.main}>
-          <div className={styles.backgroundbox}>
-            <div className={styles.dottedbox}>
-              <div className={styles.box}></div>
+          <div className={styles.Container}>
+            <div className={styles.backgroundbox}>
+              <div className={styles.dottedbox}>
+                <div className={styles.box}></div>
+              </div>
             </div>
-          </div>
-          <div className={styles.springContainer}>
-            <img src={SpringIcon} alt="Spring" className={styles.springIcon} />
-            <img src={SpringIcon} alt="Spring" className={styles.springIcon} />
-
-          </div>
-          <div className={styles.backgroundbox}>
-            <div className={styles.dottedbox}>
-              <div className={styles.box}></div>
+            <div className={styles.springContainer}>
+              <img src={SpringIcon} alt="Spring" className={styles.springIcon} />
+              <img src={SpringIcon} alt="Spring" className={styles.springIcon} />
+            </div>
+            <div className={styles.backgroundbox}>
+              <div className={styles.dottedbox}>
+                <div className={styles.box}></div>
+              </div>
             </div>
           </div>
           <Category />
 
         </main>
       </div>
-    </body>
   );
 };
 
